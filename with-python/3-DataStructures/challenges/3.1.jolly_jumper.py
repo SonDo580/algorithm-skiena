cb0a98510890e7577c0c2de650e079a74859eeb2
Son Do <dominhson580@gmail.com>
2024-10-16 00:38:14 +0700

    challenge 3.1 - jolly jumper

---
+++ with-python/3-DataStructures/challenges/3.1.jolly_jumper.py
@@ -0,0 +1,32 @@
+# A sequence of n > 0 integers is called a jolly jumper if 
+# the absolute values of the differences between successive elements 
+# take on all possible values 1 through n - 1.
+
+def is_jolly_jumper(nums):
+    n = len(nums)
+    if n == 1:
+        return True
+
+    diffs = [False] * n
+
+    for i in range(n - 1):
+        diff = abs(nums[i + 1] - nums[i])
+        
+        if diff == 0 or diff > n - 1:
+            return False
+        
+        diffs[diff] = True
+
+    return True
+
+def main():
+    inputs = [
+        [1, 4, 2, 3], # 3 2 1 -> jolly
+        [5, 1, 4, 2, -1, 6] # 4 3 2 3 5 -> not jolly (lack 1)
+    ]
+
+    for nums in inputs:
+        print(f"{'Jolly' if is_jolly_jumper(nums) else 'Not Jolly'}: {nums}")
+
+if __name__ == '__main__':
+    main()
